ce348ebc2d6ca0208c0585bd1ec5e8b73b8c9294
Shivva <matthieumariejoseph@gmail.com>
2021-12-07 22:50:12 +0100

    first commit

---
+++ cappuccino-subgraph/src/mappings/optionPoolFactoryMapping.ts
@@ -0,0 +1,25 @@
+import { log } from "@graphprotocol/graph-ts";
+import { OptionPool } from "../../generated/schema";
+import {LogOptionPoolCreation} from "../../generated/OptionPoolFactory/OptionPoolFactory";
+
+
+export function handleOptionPoolCreated(event: LogOptionPoolCreation) {
+  let entity = OptionPool.load(event.params.hash.toString());
+  if (entity != null) {
+    log.debug("Duplicate LP eject {}", [event.params.hash.toString()]);
+    return;
+  } else {
+    entity = new OptionPool(event.params.hash.toString());
+  }
+
+  entity.pool = event.params.pool.toHexString();
+  entity.base = event.params.base.toHexString();
+  entity.short = event.params.short.toHexString();
+  entity.optionType = event.params.optionType;
+  entity.liquidity = event.params.liquidity;
+  entity.bcv = event.params.bcv;
+  entity.strike = event.params.strike;
+  entity.maturity = event.params.maturity;
+
+  entity.save();
+}
---
+++ cappuccino-subgraph/src/mappings/optionPoolMapping.ts
@@ -0,0 +1,21 @@
+import { log, BigInt } from "@graphprotocol/graph-ts";
+import { Option } from "../../generated/schema";
+import {LogOptionCreation} from "../../generated/OptionPool/OptionPool";
+
+export function handleCreateOption(event: LogOptionCreation) {
+  let entity = Option.load(event.params.id.toString());
+  if (entity != null) {
+    log.debug("Duplicate Option {}", [event.params.id.toString()]);
+    return;
+  } else {
+    entity = new Option(event.params.id.toString());
+  }
+
+  entity.status = "submitted";
+  entity.pool = event.params.pool.toHexString();
+  entity.user = event.params.user.toHexString();
+  entity.amountOut = event.params.amountOut;
+  entity.amountIn = event.params.amountIn;
+
+  entity.save();
+}
